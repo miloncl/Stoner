240b9c294929d09b441de2c5fb6e18c64240690a
Milon <miloncl@gmail.com>
2018-09-15 21:56:09 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var wins = 0;
 var losses = 0;
 var green, red, blue, yellow;
-var total;
+var total = 0;
 var randomNumber;
 var gameOver = false;
 
@@ -9,6 +9,7 @@ var gameOver = false;
 function start() {
     randomNumber = Math.floor(Math.random() * 100)
     gameOver = false;
+    total = 0;
 
     $("#wins").html(wins);
     $("#losses").html(losses);
@@ -21,6 +22,7 @@ function win() {
         wins++;
         $("#wins").html(wins);
         gameOver = true;
+        total = 0;
     }
 }
 
@@ -30,7 +32,46 @@ function lose() {
         losses++;
         $("#losses").html(losses);
         gameOver = true;
+        total = 0;
     }
+}
+
+if(!gameOver){
+$(".sapphire").click( function () {
+    blue = Math.floor(Math.random() * 25);
+    total += blue;
+    $("#score-total").html(total);
+    win();
+    lose();
+})
+
+$(".ruby").click(function() {
+    red = Math.floor(Math.random() * 25);
+    total += red;
+    $("#score-total").html(total);
+    win();
+    lose();
+})
+
+$(".greenstone").click(function() {
+    green = Math.floor(Math.random() * 25);
+    total += green;
+    $("#score-total").html(total);
+    win();
+    lose();
+})
+
+$(".yellowstone").click(function() {
+    yellow = Math.floor(Math.random() * 25);
+    total += yellow;
+    $("#score-total").html(total);
+    win();
+    lose();
+})
+}
+else
+{
+    start();
 }
 
 start();
